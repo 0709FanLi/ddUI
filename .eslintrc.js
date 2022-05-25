bd5b2937895686a607d27c00811d8c3f6c7358b0
0709FanLi <385717045@qq.com>
2022-05-25 20:52:14 +0800

    feat(init): 提交规范和代码规范
    增加代码规范和提交规范

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended' // 添加 prettier 插件  解决eslint和pritter的冲突
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ddUI
 
 ## 1. 提交代码规范
+
+## 2. 代码规范
+
+npx prettier --write . 格式化代码
