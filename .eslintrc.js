245e9898cb8f6c14d9853827b56616675e2818de
0709FanLi <385717045@qq.com>
2022-05-25 22:23:47 +0800

    feat(components): feat(components): 添加vue ts
    增加代码规范和提交规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/vue3-essential',
     'airbnb-base',
     'plugin:prettier/recommended' // 添加 prettier 插件  解决eslint和pritter的冲突
   ],
