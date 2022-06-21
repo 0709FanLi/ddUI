f3f289e8b80d9ea7060faa22f4bcfef2d4722c7b
0709FanLi <385717045@qq.com>
2022-06-21 09:43:41 +0800

    feat(components): feat(components): 文档优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,10 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
-import fs from 'fs'
-import { baseParse } from '@vue/compiler-core'
+import vitePluginVue from './plugins/vue-custom-blocks-plugin'
 
 const mdPlugin = require('vite-plugin-markdown')
-
-const vitePluginVue = {
-  name: 'demo',
-  transform(code, id) {
-    if (!/\/src\/views\/doc\/.*\.demo\.vue/.test(id) || !/vue&type=demo/.test(id)) {
-      return ''
-    }
-    const pathUrl = `.${id.match(/\/src\/views\/doc\/.*\.demo\.vue/)[0]}`
-    const file = fs.readFileSync(pathUrl).toString()
-    const parsed: any = baseParse(file).children.find((n: any) => n.tag === 'demo')
-    const title = parsed.children[0].content
-    const main = file.split(parsed.loc.source).join('').trim()
-    return `export default function (Component) {
-      Component.__sourceCode = ${JSON.stringify(main)}
-      Component.__sourceCodeTitle = ${JSON.stringify(title)}
-    }`.trim()
-  }
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
